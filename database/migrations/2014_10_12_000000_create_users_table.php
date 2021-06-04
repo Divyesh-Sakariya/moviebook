469097ba38a3ebbc01d85e185ac049dcca8e39f0
Divyesh-Sakariya <divyeshsakariya0507@gmail.com>
2021-06-04 18:36:16 +0530

    New Changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('phone')->nullable();
             $table->string('city')->nullable();
             $table->string('email')->unique();
+            $table->string('role')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
